ec165e77ce53ffbd01bf037737b814b1555384c2
Marcos Castiglioni <1castiglionimarcos@gmail.com>
2025-07-17 22:31:35 -0300

    Actualizar build para deploy

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'  // Importar estilos de Bootstrap
 import './index.css'
 
-const redirect = sessionStorage.redirect;
-if (redirect) {
+if (sessionStorage.redirect) {
+  const redirect = sessionStorage.redirect;
   sessionStorage.removeItem("redirect");
   window.history.replaceState(null, "", redirect);
 }
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  base: '/personalPortfolio/',  
-  plugins: [react()],
+  base: '/personalPortfolio/',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '404.html',
+          dest: '.'  // Esto lo copia directamente a /dist
+        }
+      ]
+    })
+  ]
 })
