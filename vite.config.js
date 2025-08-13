be5ebb1759862f539c5550eb4073c55445a9e26e
Marcos Castiglioni <1castiglionimarcos@gmail.com>
2025-08-13 13:21:45 -0300

    probando netklifi

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  base: '/personalPortfolio/',
+  base: '/',  // ✅ Cambiado a raíz para Netlify
   plugins: [
     react(),
     viteStaticCopy({
       targets: [
         {
           src: '404.html',
-          dest: '.'  // Esto lo copia directamente a /dist
+          dest: '.'
         }
       ]
     })
