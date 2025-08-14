a61cbdfaab590a6e8370a18f24074270a436c346
Marcos Castiglioni <1castiglionimarcos@gmail.com>
2025-08-14 14:11:35 -0300

    finalizando detalles

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -11,30 +11,34 @@ export default function Contacto({ to, end, children, onClick }) {
           <h3 className="mb-2 text-white">Contacto</h3>
           <div className="d-flex justify-content-center gap-4">
             <a
-              href="https://github.com/tu_usuario"
+              href="https://github.com/marcoscastig"
               target="_blank"
               rel="noopener noreferrer"
               aria-label="GitHub"
               className="text-light fs-4"
+              style={{ cursor: 'pointer' }}
             >
               <i className="bi bi-github"></i>
             </a>
             <a
-              href="https://linkedin.com/in/tu_usuario"
+              href="https://linkedin.com/in/castiglioni-marcos"
               target="_blank"
               rel="noopener noreferrer"
               aria-label="LinkedIn"
               className="text-light fs-4"
+              style={{ cursor: 'pointer' }}
             >
               <i className="bi bi-linkedin"></i>
             </a>
-            <a
-              href="mailto:tuemail@dominio.com"
-              aria-label="Email"
-              className="text-light fs-4"
-            >
-              <i className="bi bi-envelope-fill"></i>
-            </a>
+           <a
+  href="https://mail.google.com/mail/?view=cm&to=1castiglionimarcos@gmail.com"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="text-light fs-4"
+  style={{ cursor: 'pointer' }}
+>
+  <i className="bi bi-envelope-fill"></i>
+</a>
           </div>
           <footer className=" text-light py-4 mt-5">
       <div className="container text-center">
